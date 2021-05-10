71dcf3e30c02863c43a158b3c1d8a5299cb48250
Beatrice Castagno <beatrice.castagno1@gmail.com>
2021-05-10 17:17:44 +0200

    Aggiunti logo, nav e card

---
+++ resources/views/parts/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+    <div class="container">
+        <div class="logo">
+            <img src="{{asset ('images/marchio-sito-test.png')}}" alt="logo-pasta-la-molisana">
+        </div>
+        <nav class="nav-main">
+            <ul class="flex">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Prodotti</a></li>
+                <li><a href="#">News</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
